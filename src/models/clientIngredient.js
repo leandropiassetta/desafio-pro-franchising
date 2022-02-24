fdc53b132815c42e50e70f963032f62ce231a235
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-02-23 21:36:03 -0300

    feat: criando camada models de ingrediente

---
+++ src/models/clientIngredient.js
@@ -0,0 +1,30 @@
+const connect = require('./connection');
+
+const createIngredients = async({ name, unitOfMeasurement, price  }) => {
+  const db = await connect();
+
+  const ingredients = await db.collection('ingredients').insertOne({
+    name, unitOfMeasurement, price
+  });
+
+  return { 
+    _id: ingredients.insertedId, name, unitOfMeasurement, price 
+  }
+}
+
+const createProducts = async({ name, image, price, ingredients }) => {
+  const db = await connect();
+
+  const product = await db.collection('products').insertOne({
+    name, image, price, ingredients
+  });
+
+  return { 
+    _id: product.insertedId, name, image, price, ingredients
+  }
+}
+
+module.exports = {
+  createIngredients,
+  createProducts
+}
